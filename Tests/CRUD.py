40b970c25ca3f55f7839a0a3976150417de7938a
Tudor42 <puscas.tudor42@gmail.com>
2021-10-27 19:53:47 +0300

    tests get_obj_data and get_obj_IDs

--- Tests/CRUD.py
+++ Tests/CRUD.py
@@ -2,12 +2,25 @@ import Logic.CRUD as di
 import Domain.inventory as iv
 
 
-def test_add_obj():
+def test_get_obj_data_list():
     inventory = iv.creaza_inventoriu()
     di.add_obj(inventory, 14, "p", "x", 2, "aaaa")
-    di.add_obj(inventory, 21, "p", "x", 2, "aaaa")
-    di.add_obj(inventory, 32, "p", "x", 2, "aaaa")
-    assert di.get_obj_IDs(inventory) == [14, 21, 32]
+    di.add_obj(inventory, 21, "d", "x", 2, "aaaa")
+    di.add_obj(inventory, 32, "s", "x", 2, "aaaa")
+    assert di.get_obj_data(inventory, 14) == [14, "p", "x", 2,
+                                              "aaaa"]
+    assert di.get_obj_data(inventory, 21) == [21, "d", "x", 2,
+                                              "aaaa"]
+    assert di.get_obj_data(inventory, 32) == [32, "s", "x", 2,
+                                              "aaaa"]
+
+
+def test_get_obj_IDs():
+    inventory = iv.creaza_inventoriu()
+    di.add_obj(inventory, 15, "p", "x", 2, "aaaa")
+    di.add_obj(inventory, 16, "p", "x", 2, "aaaa")
+    di.add_obj(inventory, 17, "p", "x", 2, "aaaa")
+    assert di.get_obj_IDs(inventory) == [15, 16, 17]
 
 
 def test_modify_obj():
@@ -33,3 +46,11 @@ def test_delete_obj():
     di.delete_obj(inventory, 10)
     di.delete_obj(inventory, 100)
     assert di.get_obj_data_str(inventory, 10) == ""
+
+
+def test_add_obj():
+    inventory = iv.creaza_inventoriu()
+    di.add_obj(inventory, 14, "p", "x", 2, "aaaa")
+    di.add_obj(inventory, 21, "p", "x", 2, "aaaa")
+    di.add_obj(inventory, 32, "p", "x", 2, "aaaa")
+    assert di.get_obj_IDs(inventory) == [14, 21, 32]
--- Tests/run_tests.py
+++ Tests/run_tests.py
@@ -7,5 +7,7 @@ def run_tests():
     cr.test_add_obj()
     cr.test_delete_obj()
     cr.test_modify_obj()
+    cr.test_get_obj_data_list()
+    cr.test_get_obj_IDs()
     td.test_obiect()
     tl.test_mutare()
